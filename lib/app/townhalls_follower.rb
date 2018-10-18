d535385b1afd8fe58fd1c5860d0a6957f71c5ced
cedricsoares <cedricsoares@me.com>
2018-10-18 19:24:47 +0200

    townhalls_follower.rb in a class

--- lib/app/townhalls_follower.rb
+++ lib/app/townhalls_follower.rb
@@ -5,55 +5,60 @@ require 'dotenv'
 
 Dotenv.load('../../.env')
 
-
 #townhalls_follower.rb permet de follow sur twitte toutes les villes du json townhalls_contact
 
 
-#méthode qui extrait le json des villes dans un hash 
-def extract_json
+class folloers 
 
-	@handles = Hash.new
-
-	json = File.read('../../townhalls_contact.json')
-   	@handles = JSON.parse(json)
-
-end 
-
-#méthode pour iniatialiser l'API Twitter 
-def twitter_init 
+	def iitialize 
+		twitter_init
+		follow_all
+	end 
 
 
-	@client = Twitter::REST::Client.new do |config|
- 		config.consumer_key = ENV["TWITTER_API_KEY"]
- 		config.consumer_secret = ENV["TWITTER_SECRET_KEY"]
-  		config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
-  		config.access_token_secret = ENV["TWITTER_ACESS_TOKEN_SECRET"]
-	end
-end
 
-#méthode qui follow tous les handles du hashs 
-def follow_all
+	
 
-	extract_json
+	#méthode qui extrait le json des villes dans un hash 
+	def extract_json
 
-	@handles.each do |departement, v_dep|
+		@handles = Hash.new
 
-		v_dep.each do |town, v_town|
-		
+		json = File.read('../../townhalls_contact.json')
+	   	@handles = JSON.parse(json)
 
-			#follow de chaque handles du hash 	
-			@client.follow("#{v_town["handle_twitter"]}")
+	end 
 
+	#méthode pour iniatialiser l'API Twitter 
+	def twitter_init 
+
+
+		@client = Twitter::REST::Client.new do |config|
+	 		config.consumer_key = ENV["TWITTER_API_KEY"]
+	 		config.consumer_secret = ENV["TWITTER_SECRET_KEY"]
+	  		config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
+	  		config.access_token_secret = ENV["TWITTER_ACESS_TOKEN_SECRET"]
 		end
+	end
 
-   	end 
+	#méthode qui follow tous les handles du hashs 
+	def follow_all
+
+		extract_json
+
+		@handles.each do |departement, v_dep|
+
+			v_dep.each do |town, v_town|
+			
+
+				#follow de chaque handles du hash 	
+				@client.follow("#{v_town["handle_twitter"]}")
+
+			end
+
+	   	end 
+
+	end
 
 end
-
-def perform
-	twitter_init
-	follow_all
-end 
-
-perform 
 
