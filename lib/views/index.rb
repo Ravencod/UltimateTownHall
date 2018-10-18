e7d5d18d0945b28a379b6bc8b69ead0114c8aa59
Ravencod <42932340+Ravencod@users.noreply.github.com>
2018-10-19 00:03:45 +0200

    Update index.rb

--- lib/views/index.rb
+++ lib/views/index.rb
@@ -3,8 +3,8 @@ class Index
   
   def set_action
     puts "Press 1 to launch the scrapper: retrieve contacts information for each townhall of each department"
-    puts "Press 2 to launch the mailer: send an email introducing THP to each tonwhall"
-    puts "Press 3 to launch the adder: retrieve the twitter hadnles of each townhall"
+    puts "Press 2 to launch the mailer: send an email introducing THP to each townhall"
+    puts "Press 3 to launch the adder: retrieve the twitter handles of each townhall"
     puts "Press 4 to launch the follower: follow on twitter each townhall"
     print '>> '
     @action = gets.chomp.to_i
