c5ebf08b71d87d74e8e1dd2d8d4b4876d7e8270e
Bapuch <43609639+Bapuch@users.noreply.github.com>
2018-10-18 23:49:20 +0200

    Update index.rb

--- lib/views/index.rb
+++ lib/views/index.rb
@@ -1,7 +1,7 @@
 class Index
   attr_accessor :action
   
-  def get_action
+  def set_action
     puts "Press 1 to launch the scrapper: retrieve contacts information for each townhall of each department"
     puts "Press 2 to launch the mailer: send an email introducing THP to each tonwhall"
     puts "Press 3 to launch the adder: retrieve the twitter hadnles of each townhall"
