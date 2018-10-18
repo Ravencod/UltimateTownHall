37b3296ff00f1f69bf86b6d9a741b199a0290ff3
Bapuch <43609639+Bapuch@users.noreply.github.com>
2018-10-18 22:48:48 +0200

    Update townhalls_mailer.rb

--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -9,16 +9,28 @@ class Mailer
   end
 
   def send_emails
+    puts "Give me your mail or press 'N' to use the default fake (but working) mail:"
+    print "> "
+    adress = gets.chomp
+    if adress.upcase == 'N'
+      adress = "hyppoleuleu@gmail.com"
+      password = "1234567huit!"
+    else
+      puts "Now, give your password mail or press 'N' to use the default fake (but working) password :"
+      print "> "
+      password = gets.chomp
+    end
+    puts password
     options = { address: 'smtp.gmail.com',
-                user_name: 'hyppoleuleu@gmail.com',
-                password: '1234567huit!',
+                user_name: adress,
+                password: password,
                 authentication: 'plain',
                 enable_starttls_auto: true }
 
     i = 0
     @contacts.values.each do |dep|
       dep.values.each do |town|
-        print ' - #{town['name'].capitalize}'
+        print " - #{town['name'].capitalize}"
         Mail.defaults do
           delivery_method :smtp, options
         end
@@ -34,7 +46,7 @@ class Mailer
             body <<-EOF
             Bonjour,
             The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de notre école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.
-            Est-ce que la mairie de #{town['name'].capitalize} #{town['email']}veut changer le monde avec nous ?
+            Est-ce que la mairie de #{town['name'].capitalize} veut changer le monde avec nous ?
             Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80
             EOF
           end
