3949f29c652ef5fa36d817e5b174baae2f75f64c
Ravencod <42932340+Ravencod@users.noreply.github.com>
2018-10-18 23:41:42 +0200

    Update townhalls_mailer.rb

--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -1,26 +1,58 @@
 require 'mail'
+require 'json'
+# Cette classe permet de gerer les mail
+class Mailer
 
-options = { :address              => "smtp.gmail.com",
-            :user_name            => 'hyppoleuleu@gmail.com',
-            :password             => '1234567huit!',
-            :authentication       => 'plain',
-            :enable_starttls_auto => true  }
+  def emails
+    json = File.read('db/townhalls_contact.json')
+    @contacts = JSON.parse(json)
+  end
 
+  def send_emails
+    puts "Give me your mail or press 'N' to use the default fake (but working) mail:"
+    print "> "
+    adress = gets.chomp
+    if adress.upcase == 'N'
+      adress = "hyppoleuleu@gmail.com"
+      password = "1234567huit!"
+    else
+      puts "Now, give your password mail or press 'N' to use the default fake (but working) password :"
+      print "> "
+      password = gets.chomp
+    end
+    puts password
+    options = { address: 'smtp.gmail.com',
+                user_name: adress,
+                password: password,
+                authentication: 'plain',
+                enable_starttls_auto: true }
 
+    i = 0
+    @contacts.values.each do |dep|
+      dep.values.each do |town|
+        print " - #{town['name'].capitalize}"
+        Mail.defaults do
+          delivery_method :smtp, options
+        end
 
-Mail.defaults do
-  delivery_method :smtp, options
-end
-
-a = 'botinatorx360@gmail.com'
-nom_commune = "LOL"
-
-Mail.deliver do
-       to a
-     from 'hyppoleuleu@gmail.com'
-  subject 'The Hacking Project'
-     body "Bonjour,
-     The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de ntore école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.
-     Déjà 500 personnes sont passées par The Hacking Project. Est-ce que la mairie de #{nom_commune} veut changer le monde avec nous ?
-     Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80"
+        Mail.deliver do
+          to 'botinatorx360@gmail.com' # town['email']
+          from 'hyppoleuleu@gmail.com'
+          charset = 'UTF-8'
+          content_transfer_encoding = '8bit'
+          subject 'The Hacking Project'
+          text_part do
+            content_type 'text/plain; charset=utf-8'
+            body <<-EOF
+            Bonjour,
+            The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de notre école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.
+            Est-ce que la mairie de #{town['name'].capitalize} veut changer le monde avec nous ?
+            Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80
+            EOF
+          end
+        end
+        i += 1
+      end
+    end
+  end
 end
